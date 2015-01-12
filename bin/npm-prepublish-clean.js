98408a510354a0ceabf6768786942426e4612a61
Matthew Andrews <matt@mattandre.ws>
2015-01-12 21:35:16 +0000

    Add prepublish/publish logic into npm scripts

---
+++ bin/npm-prepublish-clean.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+"use strict";
+
+var logger = module.exports = require('winston');
+require('es6-promise').polyfill();
+var denodeify = require('denodeify');
+var jsonFile = require('jsonfile');
+var jsonFileRead = denodeify(jsonFile.readFile);
+var jsonFileWrite = denodeify(jsonFile.writeFile);
+
+var argv = require('minimist')(process.argv.slice(2));
+
+jsonFileRead(process.cwd() + '/package.json')
+	.then(function(object) {
+		delete object.version;
+		jsonFileWrite(process.cwd() + '/package.json', object);
+	})
+	.catch(function(err) {
+		logger.error(err.toString());
+		process.exit(1);
+	});
